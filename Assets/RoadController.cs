204aa003b709d2f51907ae87327e137eb2c6c722
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-16 21:04:46 +0100

    Create road controller & write scrolling texture script to seemingly move the road

---
+++ Assets/RoadController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadController : MonoBehaviour {
+
+	public float speed;
+
+	private Vector2 offset;
+	private Renderer ren;
+
+	private void Start () {
+		ren = GetComponent<Renderer> ();
+	}
+
+
+	private void Update () {
+		offset = new Vector2 (0, speed * Time.time);
+		ren.material.mainTextureOffset = offset;
+	}
+
+}
