3fe3b53f2e77b5ee353f5364de496ccf82fc155c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-16 21:11:31 +0100

    Add player controller & clamp x movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float xSpeed;
+
+	private Vector2 pos;
+	private float xClamp = 2.2f;
+
+	private void Start () {
+		pos = transform.position;
+	}
+
+	private void Update () {
+		pos.x += Input.GetAxisRaw ("Horizontal") * xSpeed * Time.deltaTime;
+		pos.x = Mathf.Clamp (pos.x, -xClamp, xClamp);
+		transform.position = pos;
+	}
+}
