b45af2c8bbf19ad45a4fd6064da2a0d75c218d5f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-18 18:55:53 +0100

    Add enemyController and basic forward driving behaviour

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private void Update () {
+		transform.Translate (new Vector3 (0, 1, 0) * speed * Time.deltaTime);
+	}
+}
