9acb5630c2347deeeb37e91e16194bd42eb0301b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-18 20:54:19 +0100

    Add main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public void PlayButton () {
+		SceneManager.LoadScene ("Main");
+	}
+}
