1c9c12146b62d2a80b06d7f0ad048b8a1ba0774c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-08-18 19:29:12 +0100

    Create enemyCarSpawner & enemyCar prefabs

---
+++ Assets/Scripts/EnemyCarSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCarSpawner : MonoBehaviour {
+
+	public GameObject[] cars;
+	public float spawnRate = 1f;
+
+	private float xSpawnLimit = 2.2f;
+	private float timer;
+
+	private void Start () {
+		timer = spawnRate;
+	}
+	
+	private void Update () {
+		timer -= Time.deltaTime;
+
+		if (timer < 0) {
+			Vector3 spawnPos = new Vector3 (Random.Range (-xSpawnLimit, xSpawnLimit), transform.position.y, transform.position.z);
+			GameObject newCar = Instantiate(cars[Random.Range(0, cars.Length - 1)], spawnPos, Quaternion.identity) as GameObject;
+			timer = spawnRate;
+			newCar.transform.SetParent (this.transform);
+		}
+	}
+
+
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,19 @@ public class EnemyController : MonoBehaviour {
 
 	public float speed = 5f;
 
+	private SpriteRenderer sr;
+
+	private void Start () {
+		transform.localRotation = Quaternion.Euler (new Vector3 (0f, 0f, 180f));
+		transform.localScale = new Vector3 (0.8f, 0.8f, 1f);
+		Invoke ("Destroy", 4f);
+	}
+
 	private void Update () {
-		transform.Translate (new Vector3 (0, 1, 0) * speed * Time.deltaTime);
+		transform.Translate (new Vector3 (0f, 1f, 0f) * speed * Time.deltaTime);
+	}
+
+	private void Destroy () {
+		Destroy (this.gameObject);
 	}
 }
